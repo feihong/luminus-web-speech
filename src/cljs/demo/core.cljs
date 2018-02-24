0bd72b70a23c7d6d17f44e7091d259f67305d207
Feihong Hsu <feihong.hsu@gmail.com>
2018-02-23 23:09:10 -0600

    Fix typo

--- src/cljs/demo/core.cljs
+++ src/cljs/demo/core.cljs
@@ -40,8 +40,8 @@
    [:h1 "Web Speech Demo"]
    [:div "This button uses rf/dispatch and doesn't work on mobile Safari, unless you click the bottom button first"]
    [:button.btn.btn-danger {:on-click #(rf/dispatch [:say-something])}
-                           "Speak using dispatch"]                     
-   [:div "This button always works because it calls speechSynthesis.speak() directory"]
+                           "Speak using dispatch"]
+   [:div "This button always works because it calls speechSynthesis.speak() directly"]
    [:button.btn.btn-primary {:on-click #(.speak js/speechSynthesis (js/SpeechSynthesisUtterance. "Speak now or hold your peace"))}
                             "Speak immediately"]])
 
